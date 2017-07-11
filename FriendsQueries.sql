224442202d8097b445f9d3f64e9e4a17912ea2c5
Chao Charlie Huang <chaocharliehuang@gmail.com>
2017-07-11 13:02:13 -0700

    submitted form data gets added to database

---
+++ FriendsQueries.sql
@@ -0,0 +1,13 @@
+INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at)
+VALUES ("Jay", "Patel", "Instructor", NOW(), NOW());
+
+INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at)
+VALUES ("Jimmy", "Jun", "Instructor", NOW(), NOW());
+
+INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at)
+VALUES ("Charlie", "Huang", "Bum", NOW(), NOW());
+
+INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at)
+VALUES ("Emily", "Lopez", "Student", NOW(), NOW());
+
+SELECT * FROM friends;
